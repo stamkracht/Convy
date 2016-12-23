10d0afab1d83dd5bf53266a8b6b019bd1aaf8d3d
JordyPouw <jordypouw@gmail.com>
2016-12-23 14:55:12 +0100

    restructure variables

--- src/js/scopes.entry.js
+++ src/js/scopes.entry.js
@@ -5,8 +5,10 @@
 
   // the register trigger to flip the auth component.
   // and show the register form instead of login.
-  let registerButton = document.querySelector('.js-register-open'),
-      authComponent = document.querySelector('.c-authentication');
+  let authComponent = document.querySelector('.c-authentication'),
+      LoginBtn = document.querySelector('.c-authentication__submit button'),
+      registerBtnOpen = document.querySelector('.js-register-open'),
+      registerBtnClose = document.querySelector('.js-register-close');
 
   registerButton.addEventListener('click', function (e) {
     e.preventDefault();
