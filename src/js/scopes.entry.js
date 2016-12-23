09b1e553683e15ce92203ca8206c7df63ff9d155
JordyPouw <jordypouw@gmail.com>
2016-12-23 11:53:41 +0100

    rename js class register-trigger

--- src/js/scopes.entry.js
+++ src/js/scopes.entry.js
@@ -5,7 +5,7 @@
 
   // the register trigger to flip the auth component.
   // and show the register form instead of login.
-  let registerButton = document.querySelector('.js-register-trigger'),
+  let registerButton = document.querySelector('.js-register-open'),
       authComponent = document.querySelector('.c-authentication');
 
   registerButton.addEventListener('click', function (e) {
