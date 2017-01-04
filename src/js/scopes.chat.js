adc7abaee47c3f6c0fc712e8b6916f82988611bd
JordyPouw <jordypouw@gmail.com>
2017-01-04 18:44:38 +0100

    clean up chat scope

--- src/js/scopes.chat.js
+++ src/js/scopes.chat.js
@@ -4,12 +4,7 @@
 
 
   if (document.querySelector('.s-chat')) {
-    let attachmentTrigger = document.querySelector('.js-chat-attachment');
-
-    attachmentTrigger.addEventListener('click', function (e) {
-      this.classList.toggle('state-active');
-    });
-
+    // code.
   }
 
 
