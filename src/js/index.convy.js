8b7c90f7135773e75e61d97c77db30c41c8c6cd2
JordyPouw <jordypouw@gmail.com>
2017-01-04 18:44:10 +0100

    add messenger component

--- src/js/index.convy.js
+++ src/js/index.convy.js
@@ -5,6 +5,7 @@
 
   require('./components.form.js');
   require('./components.nav-more.js');
+  require('./components.messenger.js');
 
   require('./scopes.header.js');
   require('./scopes.entry.js');
