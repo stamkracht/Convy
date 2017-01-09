b5a9657dfd050b7da3a4ff76531eab0f5e36dd33
JordyPouw <jordypouw@gmail.com>
2017-01-09 23:35:36 +0100

    add react-preset to prevent task-js blowing the f up

--- gulp_tasks/task.js.js
+++ gulp_tasks/task.js.js
@@ -11,7 +11,7 @@ module.exports = function (gulp) {
       entries: 'src/js/index.convy.js',
       debug: true
     })
-      .transform(config.babelify, { presets: ['es2015', 'es2016'] })
+      .transform(config.babelify, { presets: ['es2015', 'es2016', 'react'] })
       .bundle()
       .pipe(config.vss('main.js'))
       .pipe(gulp.dest(config.source.tmp))
