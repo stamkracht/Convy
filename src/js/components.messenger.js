4bdd87d4df6c57558f2ce41537b6ec00f9123c77
JordyPouw <jordypouw@gmail.com>
2017-01-12 13:51:03 +0100

    scafold messenger component

--- src/js/components.messenger.js
+++ src/js/components.messenger.js
@@ -1,16 +1,25 @@
-(function(){
+import React from 'react';
 
-  'use strict';
+class Messenger extends React.Component {
+  render() {
+    return (
+      <article className="c-messenger">
+        <button className="c-messenger__attachment" onClick={this._toggleAttachment.bind(this)}>
+          <i className="icon-add-circle-outline"></i>
+        </button>
 
+        <textarea placeholder="Share knowledge"></textarea>
 
-  if (document.querySelector('.c-messenger')) {
-    let attachmentTrigger = document.querySelector('.js-messenger-attachment');
-
-    attachmentTrigger.addEventListener('click', function (e) {
-      this.classList.toggle('state-active');
-    });
-
+        <button className="c-messenger__submit">
+          <i className="icon-send"></i>
+        </button>
+      </article>
+    );
   }
 
+  _toggleAttachment() {
+    console.log('set class to: state-active');
+  }
+}
 
-})();
+export default Messenger;
