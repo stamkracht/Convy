72dd779de652f61bf5524515b60102af5b3e49df
JordyPouw <jordypouw@gmail.com>
2017-01-12 14:35:56 +0100

    scafold conversation scope

---
+++ src/js/scopes.conversation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Chat from './scopes.chat';
+import Header from './scopes.header';
+
+class Conversation extends React.Component {
+  render() {
+    return (
+      <main>
+        <Header/>
+        <Chat/>
+      </main>
+    );
+  }
+
+  // functions.
+}
+
+export default Conversation;
