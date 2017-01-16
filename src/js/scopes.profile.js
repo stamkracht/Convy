c8d5cb7365cb97938dc444f52864e376a5fb34a3
JordyPouw <jordypouw@gmail.com>
2017-01-16 16:48:04 +0100

    create profile scope react

---
+++ src/js/scopes.profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import ProfileSummary from './components.profile-summary';
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <section className="s-profile">
+        <ProfileSummary/>
+      </section>
+    );
+  }
+
+  // functions.
+}
+
+export default Profile;
