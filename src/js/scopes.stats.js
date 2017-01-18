ce00404b425b32373ba5920c8831e6ea6bb4fa55
JordyPouw <jordypouw@gmail.com>
2017-01-18 11:27:03 +0100

    add scafold for conversation stats scope

---
+++ src/js/scopes.stats.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Stats extends React.Component {
+  render() {
+    return (
+      <section className="s-stats">
+      </section>
+    );
+  }
+
+  // functions.
+}
+
+export default Stats;
