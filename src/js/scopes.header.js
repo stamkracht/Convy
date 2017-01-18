5307b5703ee3149a8352ee9f5b68cef615603078
JordyPouw <jordypouw@gmail.com>
2017-01-18 16:36:46 +0100

    add chat state to header component

--- src/js/scopes.header.js
+++ src/js/scopes.header.js
@@ -3,37 +3,80 @@ import React from 'react';
 import NavMore from './components.nav-more';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chatActive: true
+    };
+  }
+
   render() {
+    let navigation;
+    let headerClass;
+    let logoClass;
+    let backButtonClass;
+
+    if (this.state.chatActive) {
+      navigation = <NavChat/>;
+      headerClass = 's-header state-chat';
+      logoClass = 'logo';
+      backButtonClass = 'back-button state-active';
+    } else {
+      navigation = <NavMain/>;
+      headerClass = 's-header';
+      logoClass = 'logo state-active';
+      backButtonClass = 'back-button';
+    }
+
     return (
-      <header className="s-header state-chat">
+      <header className={headerClass}>
         <div className="icon">
-          <img className="logo" src="dest/text-icon.png" alt="Convy icon" width="30" height="30"/>
-          <a className="back-button js-back-button state-active" href="#"><i className="icon-arrow-back"></i></a>
+          <img className={logoClass} src="dest/text-icon.png" alt="Convy icon" width="30" height="30"/>
+          <a className={backButtonClass} onClick={this.closeConversation.bind(this)}><i className="icon-arrow-back"></i></a>
         </div>
 
         <span className="seperator"></span>
 
-        <nav className="c-nav-main">
-          <ul>
-            <li><a className="c-nav-main__button" href="#">Chats</a></li>
-            <li><a className="c-nav-main__button" href="#">Contacts</a></li>
-          </ul>
-        </nav>
-
-        <nav className="c-nav-main c-nav-main--chat js-nav-main-chat state-active">
-          <ul>
-            <li><a className="c-nav-main__button c-nav-main__button--chat" href="#"><i className="icon-message"></i></a></li>
-            <li><a className="c-nav-main__button c-nav-main__button--profile" href="#"><i className="icon-person"></i></a></li>
-            <li><a className="c-nav-main__button c-nav-main__button--charts" href="#"><i className="icon-bar-chart"></i></a></li>
-          </ul>
-        </nav>
+        {navigation}
 
         <NavMore/>
       </header>
     );
   }
 
-  // functions.
+  closeConversation() {
+    this.setState({
+      chatActive: false
+    });
+  }
+}
+
+class NavMain extends React.Component {
+  render() {
+    return (
+      <nav className="c-nav-main">
+        <ul>
+          <li><a className="c-nav-main__button" href="#">Chats</a></li>
+          <li><a className="c-nav-main__button" href="#">Contacts</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class NavChat extends React.Component {
+  render() {
+    return (
+      <nav className="c-nav-main c-nav-main--chat js-nav-main-chat state-active">
+        <ul>
+          <li><a className="c-nav-main__button c-nav-main__button--chat" href="#"><i className="icon-message"></i></a></li>
+          <li><a className="c-nav-main__button c-nav-main__button--profile" href="#"><i className="icon-person"></i></a></li>
+          <li><a className="c-nav-main__button c-nav-main__button--charts" href="#"><i className="icon-bar-chart"></i></a></li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Header;
