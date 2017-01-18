5f609b739866d838fd33a3e3859327da5dfcef13
JordyPouw <jordypouw@gmail.com>
2017-01-18 16:37:50 +0100

    clean code header component

--- src/js/scopes.header.js
+++ src/js/scopes.header.js
@@ -80,33 +80,3 @@ class NavChat extends React.Component {
 }
 
 export default Header;
-
-
-
-
-
-// if (document.querySelector('.s-header') && document.querySelector('.js-trigger')) {
-//   let trigger = document.querySelector('.js-trigger'),
-//       header = document.querySelector('.s-header'),
-//       navChat = document.querySelector('.js-nav-main-chat'),
-//       logo = document.querySelector('.s-header .icon .logo'),
-//       backButton = document.querySelector('.js-back-button');
-
-//   trigger.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     header.classList.toggle('state-chat');
-//     navChat.classList.toggle('state-active');
-//     logo.classList.toggle('state-active');
-//     backButton.classList.toggle('state-active');
-//     this.classList.toggle('state-active');
-//   });
-
-//   backButton.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     trigger.classList.toggle('state-active');
-//     header.classList.toggle('state-chat');
-//     navChat.classList.toggle('state-active');
-//     logo.classList.toggle('state-active');
-//     this.classList.toggle('state-active');
-//   });
-// }
