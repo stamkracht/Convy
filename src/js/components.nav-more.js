f3f4bc9f9b2be4301f1ba45058afe88334e8d36d
JordyPouw <jordypouw@gmail.com>
2017-01-18 17:35:23 +0100

    create state for nav-more component

--- src/js/components.nav-more.js
+++ src/js/components.nav-more.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class NavMore extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showNavMore: false
+    };
+  }
+
   render() {
+    let dropdownClass = `c-nav-more__dropdown ${this.state.showNavMore ? 'state-active' : ''}`;
+
     return (
       <nav className="c-nav-more">
-        <button className="c-nav-more__button js-nav-more-button">
+        <button className="c-nav-more__button" onClick={this.showNavMore.bind(this)}>
           <i className="icon-more"></i>
         </button>
-        <ul className="c-nav-more__dropdown js-nav-more-dropdown">
+
+        <ul className={dropdownClass}>
           <li><a href="#"><i className="icon-person-outline"></i>Profile</a></li>
           <li><a href="#"><i className="icon-notifications-none"></i>Notification settings</a></li>
           <li><a href="#"><i className="icon-earth"></i>Platforms</a></li>
@@ -17,22 +28,11 @@ class NavMore extends React.Component {
     );
   }
 
-  // functions.
+  showNavMore() {
+    this.setState({
+      showNavMore: !this.state.showNavMore
+    });
+  }
 }
 
 export default NavMore;
-
-
-
-
-
-// if (document.querySelector('.c-nav-more')) {
-//   let navMoreButton = document.querySelector('.js-nav-more-button'),
-//       navMoreDropdown = document.querySelector('.js-nav-more-dropdown');
-
-//   // toggling the dropdown nav on and off by pressing the nav-more button.
-//   navMoreButton.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     navMoreDropdown.classList.toggle('state-active');
-//   });
-// }
