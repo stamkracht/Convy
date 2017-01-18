a8fcdd85fe23720023915f474ab68c352404c24d
JordyPouw <jordypouw@gmail.com>
2017-01-18 13:54:27 +0100

    add attachment state to messenger component

--- src/js/components.messenger.js
+++ src/js/components.messenger.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class Messenger extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      showAttachment: false
+    };
+  }
+
   render() {
+    let messengerAttachmentClass = `c-messenger__attachment ${this.state.showAttachment ? 'state-active' : ''}`;
+
     return (
       <article className="c-messenger">
-        <button className="c-messenger__attachment" onClick={this._toggleAttachment.bind(this)}>
+        <button className={messengerAttachmentClass} onClick={this._toggleAttachment.bind(this)}>
           <i className="icon-add-circle-outline"></i>
         </button>
 
@@ -18,7 +28,9 @@ class Messenger extends React.Component {
   }
 
   _toggleAttachment() {
-    console.log('set class to: state-active');
+    this.setState({
+      showAttachment: !this.state.showAttachment
+    });
   }
 }
 
