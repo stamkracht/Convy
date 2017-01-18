fbcaab1ba9ca92689afa091e50ebdd97b35a9fd7
JordyPouw <jordypouw@gmail.com>
2017-01-19 00:04:04 +0100

    scafold states entry scope and clean code

--- src/js/scopes.entry.js
+++ src/js/scopes.entry.js
@@ -3,6 +3,14 @@ import React from 'react';
 import Authentication from './components.authentication';
 
 class Entry extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showRegister: false
+    };
+  }
+
   render() {
     return (
       <main className="s-entry">
@@ -10,45 +18,24 @@ class Entry extends React.Component {
           <img src="dest/text-logo.png" alt="Convy logo" width="284" height="73" />
         </div>
 
-        <Authentication/>
+        <Authentication showRegister={this.state.showRegister} hideRegister={this.hideRegister.bind(this)}/>
 
-        <button className="register-trigger js-register-open">Not registered? Create account.</button>
+        <button className="register-trigger" onClick={this.showRegister.bind(this)}>Not registered? Create account.</button>
       </main>
     );
   }
 
-  // functions.
+  showRegister() {
+    this.setState({
+      showRegister: true
+    });
+  }
+
+  hideRegister() {
+    this.setState({
+      showRegister: false
+    });
+  }
 }
 
 export default Entry;
-
-
-
-
-
-// if (document.querySelector('.s-entry')) {
-//   // the register trigger to flip the auth component.
-//   // and show the register form instead of login.
-//   let authComponent = document.querySelector('.c-authentication'),
-//       LoginBtn = document.querySelector('.c-authentication__submit button'),
-//       registerBtnOpen = document.querySelector('.js-register-open'),
-//       registerBtnClose = document.querySelector('.js-register-close');
-
-//   // activate the register pane.
-//   registerBtnOpen.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     authComponent.classList.add('state-register');
-//   });
-
-//   // deactivate the register pane and show login form again.
-//   registerBtnClose.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     authComponent.classList.remove('state-register');
-//   });
-
-//   // login the user to the app.
-//   LoginBtn.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     this.classList.add('state-loading');
-//   });
-// }
