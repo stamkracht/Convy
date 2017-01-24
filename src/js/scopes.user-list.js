45e24b3a34e809902c8d5ac5edfb80e787de9dcd
JordyPouw <jordypouw@gmail.com>
2017-01-24 15:17:57 +0100

    add props to user-list component

--- src/js/scopes.user-list.js
+++ src/js/scopes.user-list.js
@@ -4,19 +4,38 @@ import BlockUser from './components.block-user';
 
 class UserList extends React.Component {
   render() {
+    let users = renderUsers(this.props.users);
+
     return (
-      <section className="s-user-list">
+      <section className="s-user-list" id={ this.props.idName }>
         <article className="c-block-user"></article>
-        <BlockUser/>
-        <BlockUser/>
-        <BlockUser/>
-        <BlockUser/>
-        <BlockUser/>
+        {users}
       </section>
     );
   }
+}
 
-  // functions.
+UserList.propTypes = {
+  users: React.PropTypes.array,
+};
+
+function renderUsers(users) {
+  if (users.length > 0) {
+    return users.map((user, index) => (
+      <BlockUser
+        id={ user.id }
+        name={ user.firstName }
+        description={ user.description }
+        lastMessage={ user.lastMessage }
+        imageSource={ user.imageSource }
+        lastSeenDate={ user.lastSeenDate }
+        lastMessageDate={ user.lastMessageDate }
+        unreadMessagesLength={ user.unreadMessagesLength }
+      />
+    ));
+  }
+
+  else { return []; }
 }
 
 export default UserList;
