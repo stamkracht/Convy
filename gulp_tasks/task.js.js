842a270d10b6f66b93def7c0208630c294416024
JordyPouw <jordypouw@gmail.com>
2017-01-31 17:54:21 +0100

    remove console logs on production

--- gulp_tasks/task.js.js
+++ gulp_tasks/task.js.js
@@ -4,6 +4,7 @@ module.exports = function (gulp) {
 
 
   var config = require('./config')();
+  var compress = { compress: { drop_console: true } };
 
   gulp.task('js', function () {
 
@@ -18,7 +19,7 @@ module.exports = function (gulp) {
       .pipe(config.buffer())
       .pipe(config.plugins.sourcemaps.init({ loadMaps: true }))
         .pipe(config.plugins.rename({ suffix: '.min' }))
-        .pipe(config.env.production ? config.plugins.uglify() : config.plugins.util.noop())
+        .pipe(config.env.production ? config.plugins.uglify(compress) : config.plugins.util.noop())
       .pipe(config.plugins.sourcemaps.write('../' + config.source.tmp))
       .pipe(gulp.dest(config.source.dest));
 
