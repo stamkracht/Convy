51e144386fef4ff177caaf8b203331d823a8f2b1
JordyPouw <jordypouw@gmail.com>
2017-01-31 17:57:16 +0100

    clean up combined reducers

--- src/js/reducers.js
+++ src/js/reducers.js
@@ -1,43 +1,10 @@
 import { combineReducers } from 'redux';
 
-// reducer functions.
-const headerState = function(state, action) {
-  if (state === undefined) {
-    state = {
-      chatActive: false,
-    };
-  }
-
-  if (action.type === 'OPEN_CHAT') {
-    var newState = Object.assign({}, state, {
-      title: action.title,
-      id: action.id,
-      chatActive: true,
-    });
-
-    return newState;
-  }
-
-  if (action.type === 'CLOSE_CHAT') {
-    var newState = Object.assign({}, state, {
-      chatActive: false,
-    });
-
-    return newState;
-  }
-
-  return state;
-}
-
-// example reducer.
-const widgetReducer = function(state = {}, action) {
-  return state;
-}
+import headerReducer from './reducers.header';
 
 // combine the reducers
 const reducers = combineReducers({
-  headerState: headerState,
-  widgetState: widgetReducer,
+  headerState: headerReducer,
 });
 
 export default reducers;
