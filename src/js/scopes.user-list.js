1e3cbfa986ffd6105587cf45181df61d7f455d78
JordyPouw <jordypouw@gmail.com>
2017-02-13 13:38:27 +0100

    clean up user-list component

--- src/js/scopes.user-list.js
+++ src/js/scopes.user-list.js
@@ -7,23 +7,9 @@ import BlockUser from './components.block-user';
 class UserList extends React.Component {
   render() {
     let users = renderUsers(this.props);
-    let styles = {
-      left: `${ this.props.initialPosition }vw`,
-      transform: `translate(${ -this.props.position }vw, 0)`
-    };
-
-    if (this.props.animEnabled) {
-      styles.transition = 'transform 0.5s cubic-bezier(0.25, 0.5, 0.5, 1)';
-    }
 
     return (
-      <section
-        className="s-user-list"
-        onTouchStart={ this.props.startSlide }
-        onTouchMove={ this.props.moveSlide }
-        onTouchEnd={ this.props.endSlide }
-        style={ styles }
-      >
+      <section className="s-user-list">
         <div className="s-user-list__inner">
           <article className="c-block-user"></article>
           {users}
@@ -34,12 +20,6 @@ class UserList extends React.Component {
 }
 
 UserList.propTypes = {
-  initialPosition: React.PropTypes.number,
-  position: React.PropTypes.number,
-  animEnabled: React.PropTypes.bool,
-  startSlide: React.PropTypes.func,
-  moveSlide: React.PropTypes.func,
-  endSlide: React.PropTypes.func,
   openChat: React.PropTypes.func,
   users: React.PropTypes.array,
 };
