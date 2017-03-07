f851eb1037801dd236a74ef742b09100e6dd725c
JordyPouw <jordypouw@gmail.com>
2017-03-07 18:17:04 +0100

    prefix action file names with actions.*

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,8 +1,8 @@
-import * as chats from './chats'
-import * as contacts from './contacts'
-import * as me from './me'
-import * as header from './header'
-import * as swipeView from './swipe-view'
+import * as chats from './actions.chats'
+import * as contacts from './actions.contacts'
+import * as me from './actions.me'
+import * as header from './actions.header'
+import * as swipeView from './actions.swipe-view'
 
 export default {
   chats,
